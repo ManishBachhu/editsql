7189d95fa51f1db6b8fb76b1815226b2bee53f3c
ManishBachhu <manish.bachhu@nutanix.com>
2021-07-01 22:58:55 +0530

    Edit output.py

--- output.py
+++ output.py
@@ -8,16 +8,5 @@ i=0
 with open('output.txt', 'w') as json_file:
   for q in qlist:
     i=i+1
-    #print(q['input_seq'])  
-    #print(q['probability'])
-    #print(q['prediction'])
-    #print("=======================================")
-    json.dump(str(i) + '. ', json_file)
-    json.dump(' '.join(q['input_seq']), json_file)
-    json_file.write('\n')
-    json.dump(q['probability'], json_file)
-    json_file.write('\n')
     json.dump(' '.join(q['prediction']), json_file)
-    json_file.write('\n')
-    json.dump("===============================================================", json_file)
-    json_file.write('\n')
+    
